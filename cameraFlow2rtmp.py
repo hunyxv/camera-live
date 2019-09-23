ad622082352a9068febdc2a72710a0685c4c3ec0
wanghongning <15127860671@163.com>
2019-09-23 23:19:52 +0800

    pip source; FFPubshFlow

--- cameraFlow2rtmp.py
+++ cameraFlow2rtmp.py
@@ -1,24 +1,58 @@
 import subprocess
 import shlex
 
-def push_flow(resolution, fps, rtmp_url=None):
-    """
-    Args：
-        resolution: image or video resolution rate
-        fps：video fps
-        rtmp_url: rtmp service addr
-    Return:
-        subprocess.PIPE  stdin
-    """
-    command = "ffmpeg -y -f rawvideo -pix_fmt bgr24 -s {resolution} -r {fps} -i - -c:v libx264 -pix_fmt yuv420p -preset ultrafast -f flv {rtmp_url}".format(
-        resolution=resolution, fps=fps, rtmp_url=rtmp_url
-    )
-    """
-    -f rawvideo： 将输入格式设置为原始视频容器
-    -pix_fmt: 像素格式，opencv 默认的像素格式 是 bgr，后面将 bgr 转为 yuyv 格式
-    -preset h.264 压缩速度选择， ultrafast 是最快的 更多看：https://trac.ffmpeg.org/wiki/Encode/H.264
-    """
-    input_pipe = subprocess.Popen(
-        shlex.split(command), stdin=subprocess.PIPE
-    )
-    return input_pipe
+from urllib import parse
+from setting import RTMP, VIDEO
+
+class FFPushFlow(object):
+    def __init__(self, resolution_size, fps):
+        self.rtmp_url = RTMP.get('RTMP_ADDR') + '?' + parse.urlencode(RTMP.get('payload'))
+        self.resolution_size = resolution_size
+        self.fps = fps
+
+        self.ffp = self.push_flow()
+
+    def push_flow(self):
+        """
+        Args：
+            resolution: image or video resolution rate
+            fps：video fps
+            rtmp_url: rtmp service addr
+        Return:
+            subprocess.PIPE  stdin
+        """
+        command = "ffmpeg -y -f rawvideo -pix_fmt bgr24 -s {resolution} -r {fps} -i - -c:v libx264 -pix_fmt yuv420p -preset ultrafast -f flv {rtmp_url}".format(
+            resolution=self.resolution_size, 
+            fps=self.fps, 
+            rtmp_url=self.rtmp_url
+        )
+        """
+        -f rawvideo： 将输入格式设置为原始视频容器
+        -pix_fmt: 像素格式，opencv 默认的像素格式 是 bgr，后面将 bgr 转为 yuyv 格式
+        -preset h.264 压缩速度选择， ultrafast 是最快的 更多看：https://trac.ffmpeg.org/wiki/Encode/H.264
+        """
+        ffp = subprocess.Popen(
+            shlex.split(command), stdin=subprocess.PIPE
+        )
+        return ffp
+
+    def pid(self):
+        return self.ffp.pid
+    
+    def kill(self):
+        try:
+            self.ffp.send_signal(9)
+        except Exception as e:
+            print(e)
+            self.ffp.kill()
+
+    def is_alive(self):
+        if not self.ffp.poll():
+            return True
+        else:
+            return False
+
+    def restart(self):
+        self.kill()
+
+        self.ffp = self.push_flow()
---
+++ setting.py
@@ -0,0 +1,14 @@
+# RTMP server
+RTMP = {
+    "RTMP_ADDR": '',
+    "payload": {
+        "USERNAME": '',
+        "PASSWORD": ''
+    }
+}
+
+VIDEO = {
+    "FPS": 20,
+    "WIDTH": 704,
+    "HEIGHT": 480
+}
